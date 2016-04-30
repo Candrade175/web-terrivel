013cb1125b62f84a05eb12605f62ecd525f01435
Cassiano de Brito Andrade <cassiano.b.andrade@gmail.com>
2016-04-30 19:04:59 -0300

    Ajustando cores e adicionando comentários

--- www/js/javascript.js
+++ www/js/javascript.js
@@ -1,14 +1,29 @@
-var numPedras = document.getElementById("numPedras");
+var numPedras = document.getElementById("num_pedras");
 var eNumPedras = document.getElementById("iconeErroNumPedras");
 
-var nome = document.getElementById("nomeMonstro");
+var nome = document.getElementById("nome");
 var eNome = document.getElementById("iconeErroNome");
 
 var form = document.getElementById("formulario");
 
 var metodo = document.getElementById("metodo");
 
+var tipoPedra = document.getElementById("tipo_pedras").value;
+
+var sorteio = document.getElementById("checkbox1");
+
+var AZUL = "0D15F1";
+var AMARELO = "E6F10D";
+var INDIGO = "9065AC";
+var LARANJA = "E3921A";
+var VERDE = "1BC31E";
+var VERMELHO = "FB0622";
+var VIOLETA = "B854B8";
+
 function validarFormulario() {
+	var corCeu1 = document.getElementById("corCeu1").value;
+	var corCeu2 = document.getElementById("corCeu2").value;
+
 	//Validação de número de pedras ([0, inf[)
 	if (numPedras.value < 0 || isNaN(numPedras.value))
 	{
@@ -54,12 +69,7 @@ function validarFormulario() {
 	//Método de envio
 	form.method = metodo.value;
 
-	//Tipo de Pedra
-	var tipoPedra = document.getElementById("tipoPedra").value;
-
 	//Sorteando o tipo de pedra caso a opção 'Sim' tenha sido marcada
-	var sorteio = document.getElementById("checkbox1");
-	
 	if (sorteio.checked)
 	{
 		var x = Math.floor((Math.random() * 4));
@@ -74,7 +84,40 @@ function validarFormulario() {
 			tipoPedra = "Espinela";
 	}
 
-	console.log(tipoPedra);
 
-	return false;
+	console.log(corCeu1);
+	console.log(corCeu2);
+
+	//Cor do Céu
+	if (corCeu1 == "Azul")
+		corCeu1 = AZUL;
+	else if (corCeu1 == "Amarelo")
+		corCeu1 = AMARELO;
+	else if (corCeu1 == "Indigo")
+		corCeu1 = INDIGO;
+	else if (corCeu1 == "Laranja")
+		corCeu1 = LARANJA;
+	else if (corCeu1 == "Verde")
+		corCeu1 = VERDE;
+	else if (corCeu1 == "Vermelho")
+		corCeu1 = VERMELHO;
+	else
+		corCeu1 = VIOLETA;
+
+	if (corCeu2 == "Azul")
+		corCeu2 = AZUL;
+	else if (corCeu2 == "Amarelo")
+		corCeu2 = AMARELO;
+	else if (corCeu2 == "Indigo")
+		corCeu2 = INDIGO;
+	else if (corCeu2 == "Laranja")
+		corCeu2 = LARANJA;
+	else if (corCeu2 == "Verde")
+		corCeu2 = VERDE;
+	else if (corCeu2 == "Vermelho")
+		corCeu2 = VERMELHO;
+	else
+		corCeu2 = VIOLETA;
+
+	return true;
 }
